7394a12d97674388e4de8f359f6486d7db93c7c3
YG0811 <144429704+YG0811@users.noreply.github.com>
2023-10-06 11:26:55 +0300

    Завдання 1
    Цикл від одного до n

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        for (int n = 0; n < k; n++) {
+            System.out.println(n);
+        }
+    }
+}
